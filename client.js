d6828a588ff3e22b91d5da82871884f5cc4da56c
shreyash1706 <shreyashpawar1706@gmail.com>
2026-07-21 13:55:20 +0530

    P2P connection established

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const socket = new WebSocket('ws://localhost:8080');
 
+socket.onopen = () => console.log('Connected to signaling server cleanly!');
 // holds our stream and connection 
 let localStream;
 let peerConnection;
@@ -15,15 +16,24 @@ const localVideo = document.getElementById("local-video")
 const remoteVideo = document.getElementById('remote-video')
 
 startStreamBtn.addEventListener('click',startStream)
-connectPeerBtn.addEventListener('click',intiateCall)
+connectPeerBtn.addEventListener('click',initiateCall)
 
 async function startStream(){
 	try{
 		localStream = await navigator.mediaDevices.getDisplayMedia({
 			//put MediaConstraints later
-			video: true,
-			audio: true
+			video: { frameRate: { ideal: 60, max: 120 } },
+		        audio: {
+			echoCancellation: false,
+			noiseSuppression: false,
+			autoGainControl: false,
+			channelCount: 2 // Enforces Stereo sound instead of Mono!
+		        }
 		});
+
+		const track = localStream.getVideoTracks()[0];
+		track.contentHint = 'motion';
+
 		localVideo.srcObject = localStream;
 		console.log("stream started succcessfully!");
 	} catch (error) {
@@ -45,20 +55,64 @@ function createPeerConnection() {
 	};
 	//pick the stream and push it to remote peer
 	peerConnection.ontrack = (event) => {
-		remotevideo.srcObject = event.streams[0];
+		remoteVideo.srcObject = event.streams[0];
 	}
 
-	const localStream = await navigator.mediaDevices.getDisplayMedia({ video: { frameRate : { ideal: 60 , max : 120}}});
+	//const localStream = await navigator.mediaDevices.getDisplayMedia({ video: { frameRate : { ideal: 60 , max : 120}}});
+	if (localStream){
+		localStream.getTracks().forEach((track) => {
+			peerConnection.addTrack(track,localStream);
+		});
+	}
+}
+
+//created an offer(how to communicate and details) sign it and send it over websocket 
+async function initiateCall(){
+	createPeerConnection();
 	
-	localStream.getTracks().forEach((track) = > {
-		peerConnection.addTrack(track,localStream);
+	peerConnection.createOffer().then((offer) => peerConnection.setLocalDescription(offer)).then(() => {
+		socket.send(JSON.stringify({
+			type : 'offer',
+			sdp : peerConnection.localDescription,
+		}));
+		console.log("Offer created and sent successfully");
+	})
+	.catch((e) => {
+		console.error("Error Creating Offer", e);
 	});
 }
 
+// signaling logic - handling incoming messages 
+socket.onmessage = async (event) => {
+	const message = JSON.parse(event.data);
 
-async function initiateCall(){
+	if (message.type == 'offer'){
+		console.log("Received an offer! Creating answer...");
+
+		createPeerConnection();
+		peerConnection
+			.setRemoteDescription(message.sdp)
+			.then(()=> peerConnection.createAnswer()
+				.then((answer) => peerConnection.setLocalDescription(answer)).then(() => {
+					socket.send(JSON.stringify({
+						type : 'answer',
+						sdp : peerConnection.localDescription,
+					}));
+				}))
+
+	} else if (message.type == 'answer') { 
+		console.log("Received an answer! Establishing connection...");
+		peerConnection.setRemoteDescription(message.sdp);
+
+	} else if (message.type == 'ice-candidate'){
+		console.log("Received an ICE candidate!");
+		peerConnection.addIceCandidate(message.candidate).catch((e) => {
+	      console.log(`Failure during addIceCandidate(): ${e.name}`);
+	    });
+	}
+};
 
 
+	
 
-socket.onopen = () => console.log('Connected to signaling server cleanly!');
-socket.onmessage = (event) => console.log('Received from peer:', event.data);
+
