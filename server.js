2640b445b9615a589a4b3f8b872968ceb6872613
shreyash1706 <shreyashpawar1706@gmail.com>
2026-07-22 12:14:50 +0530

    ready for deployment

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import WebSocket, { WebSocketServer} from 'ws';
 
+const PORT = process.env.PORT || 8080;
+
 const wss = new WebSocketServer({
-	port: 8080
+	port: PORT
 });
+console.log(`Signaling server running on port ${PORT}`);
 
 wss.on('connection', function connection(ws) {
 	ws.on('error', console.error);
